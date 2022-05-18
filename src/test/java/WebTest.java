d4da1c077f6638413dc4883a6fe1163c5b7c8931
xbrookx <cricket666@yandex.ru>
2022-05-18 08:55:54 +0300

    added testHeading and testLastMenuItem

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -7,43 +7,88 @@ import org.testng.annotations.Test;
 
 
 public class WebTest {
-  //
+    //
     @Test
     public void testMenuStartTitle() throws InterruptedException {
 
-      String chromeDriver = "webdriver.chrome.driver";
-      String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
-      String url = "http://www.99-bottles-of-beer.net/";
-      String expectedResult = "Welcome to 99 Bottles of Beer";
+        String chromeDriver = "webdriver.chrome.driver";
+        //String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
+        String driverPath = "D:\\chromedriver.exe";
+        String url = "http://www.99-bottles-of-beer.net/";
+        String expectedResult = "Welcome to 99 Bottles of Beer";
 
-      System.setProperty(chromeDriver, driverPath);
-      WebDriver driver = new ChromeDriver();
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
 
-      driver.get(url);
-      WebElement menuBrowseLanguages = driver.findElement(
-              By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']")
-      );
+        driver.get(url);
+        WebElement menuBrowseLanguages = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']")
+        );
 
-      menuBrowseLanguages.click();
-
-
-
-
-      WebElement menuStart = driver.findElement(
-              By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/']")
-      );
-
-      menuStart.click();
-
-      WebElement h2 = driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/h2"));
-
-      String actualResult = h2.getText();//получить текс из хпаф
-
-      Assert.assertEquals(actualResult,expectedResult);
+        menuBrowseLanguages.click();
 
 
-      driver.quit();
+        WebElement menuStart = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/']")
+        );
 
+        menuStart.click();
+
+        WebElement h2 = driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/h2"));
+
+        String actualResult = h2.getText();//получить текс из хпаф
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+
+        driver.quit();
 
     }
+
+    @Test
+    public static void testHeading() {
+
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "D:\\chromedriver.exe";
+        String url = "http://www.99-bottles-of-beer.net/";
+        String expectedResult = "99 Bottles of Beer";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+        WebElement header = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='header']/h1")
+        );
+
+        String actualResult = header.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+    }
+
+    @Test
+    public static void testLastMenuItem() {
+
+        String chromeDriver = "webdriver.chrome.driver";
+        //String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
+        String driverPath = "D:\\chromedriver.exe";
+        String url = "http://www.99-bottles-of-beer.net/";
+        String expectedResult = "Submit new Language";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+        WebElement lastMenuItemName = driver.findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']" +
+                        "/li[last()]/a[@href='/submitnewlanguage.html']")
+        );
+
+        String actualResult = lastMenuItemName.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+
 }
