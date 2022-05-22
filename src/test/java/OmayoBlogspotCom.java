e3548834c70f46cfcbdbf0d9951fcb901b848168
Valentin <cricket666@yandex.ru>
2022-05-22 17:35:24 +0300

    continue

--- src/test/java/OmayoBlogspotCom.java
+++ src/test/java/OmayoBlogspotCom.java
@@ -1,7 +1,6 @@
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -9,6 +8,7 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 public class OmayoBlogspotCom {
+
     public static final String URL = "http://omayo.blogspot.com/";
 
     @Test
@@ -193,7 +193,7 @@ public class OmayoBlogspotCom {
     }
 
     @Test
-    public static void testsenKeysOfAlert() { //
+    public static void testSendKeysOfAlert() { //ввести текст в окне предупреждения
         String chromeDriver = "webdriver.chrome.driver";
         String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
         System.setProperty(chromeDriver, driverPath);
@@ -203,6 +203,131 @@ public class OmayoBlogspotCom {
         driver.findElement(By.id("prompt")).click();
         Alert alert = driver.switchTo().alert();
         alert.sendKeys("Aurm"); //Ввод текста, проблемы с водом в браузере хром.
-        
+    }
+
+    @Test
+    public static void testSendKeysOfWebElementAndSendKeysOfActions() { //ввести текст в окне предупреждения
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+        driver.get(URL);
+
+
+        WebElement userNameField = driver.findElement(By.name("userid"));
+        userNameField.sendKeys("Arum"); // Для ввода текса в текстовые поля
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.TAB).build().perform(); // Для имитации клавиш клавиатуры
+        WebElement passwordField = driver.findElement(By.name("pswrd"));
+        passwordField.sendKeys("password"); // Для ввода текса в текстовые поля
+        actions.sendKeys(Keys.TAB).sendKeys(Keys.ENTER).build().perform(); // Для имитации клавиш клавиатуры
+    }
+
+    @Test
+    public static void testDragAndDropBy() { //передвинуть ползунок
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://omayo.blogspot.com/p/page3.html");
+
+        WebElement minPriceOption = driver.findElement(By.xpath("//a[@aria-labelledby='price-min-label']"));
+        Actions actions = new Actions(driver);
+        actions.dragAndDropBy(minPriceOption, 100,0)// выбираем элемент который хотим передвинуть по горизонтали, на 100 пунктов, не привязано к шкале, y - вертикаль.
+                .build() // Используется если действий  > 1 ->(sendKeys(Keys.Tab).sendKeys(Keys.ENTER)) IF <= 1 тогда build() не требуется, используем сразу .perform()
+                .perform(); // Команда выполнить,
+    }
+
+    @Test
+    public static void testDragAndDrop() { //перетягивание элемента из точки А в точку Б
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html");
+
+        WebElement oslo = driver.findElement(By.id("box1"));
+        WebElement norway = driver.findElement(By.id("box101"));
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(oslo, norway).build().perform();
+    }
+
+    @Test
+    public static void testDragAndDropClickHold() { //перетягивание элемента с помощью мыши
+
+    }
+
+
+
+    @Test
+    public static void testContextClickAndDoubleClick() { //нажать правую кнопку мыши, сделать двойное нажатие мыши
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+        driver.get(URL);
+
+        WebElement searchBoxField = driver.findElement(By.name("q"));
+        Actions actions = new Actions(driver);
+        actions.contextClick(); //Если просто нажать ПКМ где находится курсор
+        actions.contextClick(searchBoxField).build().perform(); // Нажать ПКМ по элементу
+
+        WebElement doubleClick = driver.findElement(By.id("testdoubleclick"));
+        actions.doubleClick(doubleClick); // двойное нажатие по вэб элементу
+    }
+
+    @Test
+    public static void testKeyDownAndClick() { //Зажать кнопку на клавиатуры и кликнуть мышью
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+        driver.get(URL);
+
+        WebElement compendiumDevLink = driver.findElement(By.linkText("compendiumdev"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(compendiumDevLink).keyDown(Keys.CONTROL).click(); //Передвигается к элементу, зажимаем клавишу CONTROL и делаем click
+    }
+
+    @Test
+    public static void testPressSeveralKeys() throws InterruptedException { //Одновременное нажатие нескольких клавиш
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+        driver.get(URL);
+
+        driver.findElement(By.name("userid")).sendKeys("word");
+        Thread.sleep(3000);
+        driver.findElement(By.name("userid")).sendKeys(Keys.chord(Keys.CONTROL, "z"));
+        // Вызываем метод ввода текста, chord - возможность зажать две клавиши
+
+    }
+
+    @Test
+    public static void testJavascriptExecutor() { //Запуск JavaStript отобразить предупреждение
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+        driver.get(URL);
+
+        JavascriptExecutor jse = (JavascriptExecutor)driver;// Интерфейск исполнитель яваскрипта
+        jse.executeScript("alert('Arum Motoori');"); //Отобразить предупреждение
+    }
+
+    @Test
+    public static void testExecuteAsyncScript() { //Команда синхранизации (Выполнить синхронный код яваскрипт)
+        //Нормальный тип ЯваСкрипт - выполняется поочередно
+        //Синхронный тип яваСкрипт - тип основан на времени, первым выполняется тот который можно выполнить быстрее
+
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+        driver.get(URL);
+
+        JavascriptExecutor jse = (JavascriptExecutor)driver;// Интерфейск исполнитель яваскрипта
+        jse.executeAsyncScript("window.setTimeout(function(){alert('world');},4000);alert('Hello');"); //Сначала выполнится Хэлло, потом ворлд
     }
 }
