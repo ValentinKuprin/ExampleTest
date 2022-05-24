8fa82cc80c8f3baa959ec5a900b0a89f70b64ab7
xbrookx <cricket666@yandex.ru>
2022-05-24 08:09:17 +0300

    refactored task12

--- src/test/java/TC12.java
+++ src/test/java/TC12.java
@@ -18,44 +18,6 @@ import java.util.Arrays;
 
  *
  */
-public class TC12 {
-
-    @Test //TC_11_12
-    public static void isEmptyField() {
-        String chromeDriver = "webdriver.chrome.driver";
-        //String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
-        String driverPath = "D:\\chromedriver.exe";
-        String url = "http://www.99-bottles-of-beer.net/submitnewlanguage.html";
-        String expectedResult = "Error";
-        String expectedResult1 = "Precondition";
-        String expectedResult2 = "failed";
-        String expectedResult3 = "Incomplete";
-        String expectedResult4 = "Input";
-        String expectedResult5 = ":";
-        String expectedResult6 = "-";
-        String expectedResult7 = ".";
-        String expectedResult8 = "Error: Precondition failed - Incomplete Input.";
-
-        System.setProperty(chromeDriver, driverPath);
-        WebDriver driver = new ChromeDriver();
-
-        driver.get(url);
-        driver.findElement(
-                        By.xpath("//body/div[@id='wrap']/div[@id='main']/form[@id='addlanguage']/p/input[@type='submit']"))
-                .click();
-        WebElement errorEmptyConfirmField = driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/p"));
-        String errorMessage = errorEmptyConfirmField.getText().trim();
-        System.out.println(errorMessage);
-
-        for (int i = 0; i < errorMessage.length(); i++) {
-            if ()
-
-        }
-
-
-        Assert.assertTrue(Character.toString(errorMessage.charAt(0)).equals(Character.toString(expectedResult.charAt(0)))); //E
-
-
 
 
 
--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -280,50 +280,47 @@ public class WebTest {
 
         //Assert.assertTrue(false);
 
-
         driver.quit();
     }
 
+    public class TC12 {
 
-    @Test //TC_11_12
-    public void isEmptyField() {
-        String chromeDriver = "webdriver.chrome.driver";
-        //String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
-        String driverPath = "D:\\chromedriver.exe";
-        String url = "http://www.99-bottles-of-beer.net/submitnewlanguage.html";
-        String expectedResult = "Error: Precondition failed - Incomplete Input.";
+        @Test //TC_11_12
+        public static void isEmptyField() {
+            String chromeDriver = "webdriver.chrome.driver";
+            //String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
+            String driverPath = "D:\\chromedriver.exe";
+            String url = "http://www.99-bottles-of-beer.net/submitnewlanguage.html";
+            String expectedResult = "Error";
+            String expectedResult1 = "Precondition";
+            String expectedResult2 = "failed";
+            String expectedResult3 = "Incomplete";
+            String expectedResult4 = "Input";
+            String expectedResult5 = ":";
+            String expectedResult6 = "-";
+            String expectedResult7 = ".";
+            String expectedResult8 = "Error: Precondition failed - Incomplete Input.";
 
-        System.setProperty(chromeDriver, driverPath);
-        WebDriver driver = new ChromeDriver();
+            System.setProperty(chromeDriver, driverPath);
+            WebDriver driver = new ChromeDriver();
 
-        driver.get(url);
-        driver.findElement(
-                        By.xpath("//body/div[@id='wrap']/div[@id='main']/form[@id='addlanguage']/p/input[@type='submit']"))
-                .click();
-        WebElement errorEmptyConfirmField = driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/p"));
-        String errorMessage = errorEmptyConfirmField.getText();
-        System.out.println(errorMessage);
+            driver.get(url);
+            driver.findElement(
+                            By.xpath("//body/div[@id='wrap']/div[@id='main']/form[@id='addlanguage']/p/input[@type='submit']"))
+                    .click();
+            WebElement errorEmptyConfirmField = driver.findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/p"));
+            String errorMessage = errorEmptyConfirmField.getText().trim();
 
-        if (errorMessage.contains(":") && errorMessage.contains("-") && errorMessage.contains(".")) {
-        }
-        String[] errorWords = errorMessage.replace(":", "")
-                .replace("- ", "").replace(".", "").split(" ");
-        System.out.println(Arrays.toString(errorWords));
+            Assert.assertEquals(Character.toString(errorMessage.charAt(0)), (Character.toString(expectedResult8.charAt(0))));
+            Assert.assertEquals(Character.toString(errorMessage.charAt(7)), (Character.toString(expectedResult8.charAt(7))));
+            Assert.assertEquals(Character.toString(errorMessage.charAt(20)), (Character.toString(expectedResult8.charAt(20))));
+            Assert.assertEquals(Character.toString(errorMessage.charAt(24)), (Character.toString(expectedResult8.charAt(24))));
+            Assert.assertEquals(Character.toString(errorMessage.charAt(35)), (Character.toString(expectedResult8.charAt(35))));
+            Assert.assertTrue(errorMessage.contains(":"));
+            Assert.assertTrue(errorMessage.contains("-"));
+            Assert.assertTrue(errorMessage.contains("."));
 
-        String[] testWords = {"Error", "Precondition", "failed", "Incomplete", "Input"};
-
-        Boolean result;
-        for (int i = 0; i < errorWords.length; i++) {
-            if (Character.toString(errorWords[i].charAt(0))
-                    .equals(Character.toString(testWords[i].charAt(0)))) {
-                result = true;
-            } else {
-                result = false;
-            }
-            Assert.assertTrue(result);
-
-        }
-        driver.quit();
+            driver.quit();
     }
 
     @Test //TC_11_13
@@ -421,7 +418,7 @@ public class WebTest {
         driver.quit();
     }
 
-    @Test //TC_11_24
+    @Test //TC_11_24 // поправить асерты
     public void confirmWarningWhiteLetters() {
         String chromeDriver = "webdriver.chrome.driver";
         String driverPath = "C:\\Users\\xBrooKx\\Downloads\\chromedriver_win32\\chromedriver.exe";
@@ -457,14 +454,5 @@ public class WebTest {
         Assert.assertTrue(result, "предупреждение IMPORTANT: " +
                 "написано белыми буквами bold шрифтом на красном фоне и все буквы - capital");
 
-
-
-
-
-
-
     }
-
-
-
 }
